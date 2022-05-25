b8e445e593dd05c3c6a22a15ab2ecb3c60b3204d
chaoticgoodpanda <lwinm@seas.upenn.edu>
2022-05-24 21:15:40 -0400

    Wired up final unit test for contract

--- boilerplate-inbox/inbox/test/Lottery.test.js
+++ boilerplate-inbox/inbox/test/Lottery.test.js
@@ -81,5 +81,53 @@ describe('Lottery Contract', () => {
         assert.equal(accounts[2], players[2]);
         assert.equal(3, players.length);
     });
+
+    it('requires a minimum amount of ether to enter', async () => {
+       try {
+           await lottery.methods.enter().send({
+               from: accounts[0],
+               value: 0,
+           });
+           assert(false);
+       } catch (err) {
+           // just checks to see that some value is passed into this function, i.e. that there's an error present
+           assert(err);
+       }
+
+    });
+
+    it('only manager can call pickWinner', async () => {
+        try {
+            await lottery.methods.pickWinner().send({
+                from: accounts[1]
+            });
+            assert(false);
+        } catch (err) {
+            assert(err);
+        }
+    });
+
+    it('sends money to the winner and resets the players array', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('2', 'ether'),
+        });
+
+        // returns the amount of ether in units of Wei that a given account controls
+        // can be done for external accounts and contract accounts, works for any address you want
+        const initialBalance = await web3.eth.getBalance(accounts[0]);
+
+        // since accounts[0] also sent in the 2 eth, they should get back the 2 eth they sent in
+        await lottery.methods.pickWinner().send({from: accounts[0]});
+
+        // retrieve the final balance
+        const finalBalance = await web3.eth.getBalance(accounts[0]);
+
+        // get the difference, but however even correct diff shouldn't be exactly 2 eth due to gas fees
+        // diff b/w initial & final balance is 2 eth + gas fees, so slightly less than 2 eth
+        const difference = finalBalance - initialBalance;
+        // 1.8 is allowing for some amount of gas cost
+        assert(difference > web3.utils.toWei('1.8', 'ether'));
+    });
 });
 
