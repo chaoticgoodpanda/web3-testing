b0f0b708da12445e7eccbf409c89414fa0ab8238
chaoticgoodpanda <lwinm@seas.upenn.edu>
2022-06-03 00:23:51 -0400

    Server-side rendering basics work

--- kickstart/pages/index.js
+++ kickstart/pages/index.js
@@ -2,15 +2,14 @@ import React, { Component } from "react";
 import factory from "../ethereum/factory";
 
 class CampaignIndex extends Component {
-  async componentDidMount() {
-    // retrieve array of addresses of all deployed campaigns
+  static async getInitialProps() {
     const campaigns = await factory.methods.getDeployedCampaigns().call();
 
-    console.log(campaigns);
+    return { campaigns };
   }
 
   render() {
-    return <div>Campaigns Index!</div>;
+    return <div>{this.props.campaigns[0]}</div>;
   }
 }
 
