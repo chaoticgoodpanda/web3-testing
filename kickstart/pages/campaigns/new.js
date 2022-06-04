52d3722a4a98a6d4620f5f2ab16ac70174320e3f
chaoticgoodpanda <lwinm@seas.upenn.edu>
2022-06-04 14:37:09 -0400

    Wired up NextJS routing and account creation

--- kickstart/pages/campaigns/new.js
+++ kickstart/pages/campaigns/new.js
@@ -1,62 +1,59 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Layout from "../../components/Layout";
 import { Button, Form, Input, Message } from "semantic-ui-react";
 import factory from "../../ethereum/factory";
 import web3 from "../../ethereum/web3";
+import { useRouter } from "next/router";
 
-export default class CampaignNew extends Component {
-  state = {
-    minimumContribution: "",
-    errorMessage: "",
-    loading: false,
-  };
+export default function CampaignNew(props) {
+  const [minimumContribution, setMinimumContribution] = useState(0);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
 
-  onSubmit = async (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
 
     // start loading spinner, reset the error message in case earlier error
-    this.setState({ loading: true, errorMessage: "" });
+    setLoading(true);
+    setErrorMessage("");
 
     try {
       const accounts = await web3.eth.getAccounts();
       // minimumContribution is available in state that we've been tracking from our users in the form
-      await factory.methods
-        .createCampaign(this.state.minimumContribution)
-        .send({
-          // take advantage of Metamask feature to auto-calculate amount of gas we need, so we don't have to specify gas amount
-          // need to specify source account sending transaction in
-          from: accounts[0],
-        });
+      await factory.methods.createCampaign(minimumContribution).send({
+        // take advantage of Metamask feature to auto-calculate amount of gas we need, so we don't have to specify gas amount
+        // need to specify source account sending transaction in
+        from: accounts[0],
+      });
+
+      router.push("/");
     } catch (err) {
-      this.setState({ errorMessage: err.message });
+      setErrorMessage(err.message);
     }
 
     // stop the spinner
-    this.setState({ loading: false });
+    setLoading(false);
   };
 
-  render() {
-    return (
-      <Layout>
-        <h3>Create a Campaign</h3>
-        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
-          <Form.Field>
-            <label>Minimum Contribution</label>
-            <Input
-              label="wei"
-              labelPosition="right"
-              value={this.state.minimumContribution}
-              onChange={(event) =>
-                this.setState({ minimumContribution: event.target.value })
-              }
-            />
-          </Form.Field>
-          <Message error header="Oops!" content={this.state.errorMessage} />
-          <Button loading={this.state.loading} primary>
-            Create
-          </Button>
-        </Form>
-      </Layout>
-    );
-  }
+  return (
+    <Layout>
+      <h3>Create a Campaign</h3>
+      <Form onSubmit={onSubmit} error={!!errorMessage}>
+        <Form.Field>
+          <label>Minimum Contribution</label>
+          <Input
+            label="wei"
+            labelPosition="right"
+            value={minimumContribution}
+            onChange={(event) => setMinimumContribution(event.target.value)}
+          />
+        </Form.Field>
+        <Message error header="Oops!" content={errorMessage} />
+        <Button loading={loading} primary>
+          Create
+        </Button>
+      </Form>
+    </Layout>
+  );
 }
