63fb0f9a0cb6c26398115added616db538bccd4b
chaoticgoodpanda <lwinm@seas.upenn.edu>
2022-06-06 13:35:56 +0200

    Can add approver + contribution in ContributeForm.js

--- kickstart/pages/campaigns/[id]/index.js
+++ kickstart/pages/campaigns/[id]/index.js
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
 import Layout from "../../../components/Layout";
 import Campaign from "../../../ethereum/campaign";
-import { Card } from "semantic-ui-react";
+import { Card, Grid } from "semantic-ui-react";
+import web3 from "../../../ethereum/web3";
+import ContributeForm from "../../../components/ContributeForm";
 
 class CampaignShow extends Component {
-  static async getInitialProps(address) {
-    console.log(address);
-    const campaign = Campaign(address.query.id);
+  static async getInitialProps(props) {
+    const campaign = Campaign(props.query.id);
 
     const summary = await campaign.methods.getSummary().call();
 
     return {
+      address: props.query.id,
       minimumContribution: summary[0],
       balance: summary[1],
       requestsCount: summary[2],
@@ -36,6 +38,29 @@ class CampaignShow extends Component {
           "The manager created this campaign and can create requests to withdraw money",
         style: { overflowWrap: "break-word" },
       },
+      {
+        header: minimumContribution,
+        meta: "Minimum Contribution (wei)",
+        description:
+          "You must contribute at least this much wei to become an approver",
+      },
+      {
+        header: requestsCount,
+        meta: "Number of Requests",
+        description: "A request tries to withdraw money from the contract",
+      },
+      {
+        header: approversCount,
+        meta: "Number of Approvers",
+        description:
+          "Number of people who have already donated to this campaign",
+      },
+      {
+        header: web3.utils.fromWei(balance, "ether"),
+        meta: "Balance of Contract (ether)",
+        description:
+          "Balance is how much money the campaign has left to spend.",
+      },
     ];
 
     return <Card.Group items={items} />;
@@ -45,7 +70,12 @@ class CampaignShow extends Component {
     return (
       <Layout>
         <h3>Campaign Show</h3>
-        {this.renderCards()}
+        <Grid>
+          <Grid.Column width={10}>{this.renderCards()}</Grid.Column>
+          <Grid.Column width={6}>
+            <ContributeForm address={this.props.address} />
+          </Grid.Column>
+        </Grid>
       </Layout>
     );
   }
